84a5030f4b87698854beb1006e2dc95b1c11f157
Jill Short <jillian.b.short@gmail.com>
2020-07-10 04:13:28 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,4 @@
-def countdown (number=10)
+def countdown (number)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
@@ -6,10 +6,10 @@ def countdown (number=10)
     "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep (number=5)
-  while number <= 5
+def countdown_with_sleep (number)
+  while number > 0
     puts "#{number} SECOND(S)!"
-    number -= 1
+    sleep 1 number -= 1
   end
 end
   
